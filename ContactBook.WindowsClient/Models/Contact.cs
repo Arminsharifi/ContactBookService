c5ba592712e6f7da0befd2404e3c0b8bde17ec17
Arminsharifi4@gmail.com <Arminsharifi4@gmail.com>
2022-09-04 09:56:37 +0430

    Add Windows Client Project

---
+++ ContactBook.WindowsClient/Models/Contact.cs
@@ -0,0 +1,20 @@
+﻿namespace ContactBook.Razor.UI.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public Contact(string fullName, string phoneNumber)
+        {
+            FullName = fullName;
+            PhoneNumber = phoneNumber;
+        }
+
+        public Contact()
+        {
+
+        }
+    }
+}
